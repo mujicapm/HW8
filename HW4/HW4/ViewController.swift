52125ca425d5511df033e7dcaaa6ed015353246f
Mauro III <mauro.mujica.3@gmail.com>
2022-02-06 14:00:43 -0600

    Testing GitHub integration

--- HW4/HW4/ViewController.swift
+++ HW4/HW4/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var powerStatus: UILabel!
+    @IBOutlet weak var volumeValue: UILabel!
+    @IBOutlet weak var channelValue: UILabel!
+    @IBOutlet weak var powerSwitch: UISwitch!
+    @IBOutlet weak var volumeSlider: UISlider!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        powerStatus.text = (powerSwitch.isOn ? "on" : "off")
+        volumeValue.text = "\(Int(volumeSlider.value))"
     }
 
-
+    @IBAction func togglePower(_ sender: UISwitch) {
+        powerStatus.text = (sender.isOn ? "on": "off")
+    }
+    
+    @IBAction func volumeChange(_ sender: UISlider) {
+        volumeValue.text = "\(Int(sender.value))"
+    }
 }
 
